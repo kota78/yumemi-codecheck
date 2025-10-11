33b75654a20241d5c3c4a9761984ed9c2e022dc7
kota78 <kota1725@gmail.com>
2025-10-11 13:45:55 +0900

    リポジトリ一覧を表示するviewを追加

---
+++ lib/views/top/repository_list_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RepositoryListView extends StatelessWidget {
+  const RepositoryListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final items = List<String>.generate(10, (i) => 'Repository #${i + 1}');
+    return ListView.separated(
+      itemCount: items.length,
+      separatorBuilder: (_, __) => const Divider(height: 1),
+      itemBuilder: (context, index) => ListTile(title: Text(items[index])),
+    );
+  }
+}
