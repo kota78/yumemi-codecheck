79b26ab01526b991c2a1183dadf73c2a6337f6c4
kota78 <kota1725@gmail.com>
2025-10-15 04:11:50 +0900

    flutter_localizationsの導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:yumemi_codecheck/l10n/app_localizations.dart';
 import 'package:yumemi_codecheck/views/search/search_page.dart';
 
 void main() async {
@@ -16,6 +18,16 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // 英語
+        Locale('ja'), // 日本語
+      ],
       home: const SearchPage(),
     );
   }
