81368eb48c4c662a42ac9f2efe481e2d0c9f5c58
kota78 <kota1725@gmail.com>
2025-10-14 18:24:30 +0900

    DioClientをプロバイダーから取得する方式に変更→アクセストークンの付与のため

--- lib/repositories/github/repo_search_repository_provider.dart
+++ lib/repositories/github/repo_search_repository_provider.dart
@@ -1,12 +1,12 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:yumemi_codecheck/core/dio_client.dart';
+import 'package:yumemi_codecheck/core/dio_client_provider.dart';
 import 'package:yumemi_codecheck/repositories/github/repo_search_repository.dart';
 
 part 'repo_search_repository_provider.g.dart';
 
 @riverpod
 RepoSearchRepository repoSearchRepository(Ref ref) {
-  final dioClient = DioClient('https://api.github.com');
+  final dioClient = ref.watch(dioClientProvider);
   return RepoSearchRepository(dioClient: dioClient);
 }
